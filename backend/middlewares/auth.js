b18d6b2c07d26c7483f473984c4cd16aa4fa252f
Ilya Novik <ilya.novikk@gmail.com>
2023-01-27 22:54:31 +0800

    refactor: refactor auth

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -5,13 +5,12 @@ const { NODE_ENV, JWT_SECRET } = require('../config/config');
 
 // eslint-disable-next-line consistent-return
 const auth = (req, res, next) => {
-  const { authorization } = req.headers;
-  if (!authorization) {
-    return res.status(401).send({ message: 'Необходима авторизация' });
-  }
-  const token = authorization.replace('Bearer ', '');
-  console.log(token);
-  // const token = req.cookies.jwt;
+  // const { authorization } = req.headers;
+  // if (!authorization) {
+  //   return res.status(401).send({ message: 'Необходима авторизация' });
+  // }
+  // const token = authorization.replace('Bearer ', '');
+  const token = req.cookies.jwt;
   let payload;
   try {
     payload = jwt.verify(token, 'secret-key');
